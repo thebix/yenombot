86035bc470958fd548b6125ceac5f128e9e6b02c
Paul Byron <syntheticforms@gmail.com>
2017-05-14 14:28:53 +0300

    Time class dependency fix

--- dist/lib/index.js
+++ dist/lib/index.js
@@ -4,12 +4,12 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 
-var _Time = require('./Time');
+var _time = require('./time');
 
-var _Time2 = _interopRequireDefault(_Time);
+var _time2 = _interopRequireDefault(_time);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 exports.default = {
-    time: new _Time2.default()
+    time: new _time2.default()
 };
