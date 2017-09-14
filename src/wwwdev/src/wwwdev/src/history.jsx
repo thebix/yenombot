b867cc05a57b0a48b3f4df77557258814bea114e
Pavel Biriukov <pbiryukov@beeline.ru>
2017-09-14 10:03:14 +0300

    www history edit

--- src/wwwdev/src/wwwdev/src/history.jsx
+++ src/wwwdev/src/wwwdev/src/history.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import classNames from 'classnames'
+import { Parser } from 'expr-eval'
 
 import { CheckBoxStatefull, CheckBoxStateless } from './components/CheckBox.jsx'
 import Dynamic from './components/Dynamic.jsx'
-import Input from './components/Input.jsx'
+import Input, { InputStatefull } from './components/Input.jsx'
+import Select from './components/Select.jsx'
 
 import '../css/history.scss'
 
@@ -17,7 +19,11 @@ import {
     historyCategoryToggle,
     historyCategoriesSelected,
     historyUserToggle,
-    historyDateSet
+    historyDateSet,
+    historyEditSwitch,
+    historySave,
+    historySaveUndo,
+    historyShouldUpdate
 } from './actions'
 
 import Time from '../../../../lib/lib/time' // INFO: bad reference
@@ -47,6 +53,7 @@ const fetchHistory = ({
     l.d('History:fetchHistory()')
     // TODO: move this request to other lyfecycle method
     dispatch(usersFetch())
+    dispatch(historyEditSwitch())
     const { dateStart, dateEnd } = selectedDates
     const start = checkDateInput(dateStart)
     let end = checkDateInput(dateEnd)
@@ -71,7 +78,10 @@ export default connect(state => ({
     categories: state.historyCategories,
     selectedCategories: state.historySelectedCategories,
     selectedUsers: state.historySelectedUsers,
-    selectedDates: state.historySelectedDates
+    selectedDates: state.historySelectedDates,
+    historyEditId: state.historyEditId,
+    historyEditUndo: state.historyEditUndo,
+    historyUpdate: state.historyUpdate
 }))(class History extends Component {
     constructor(props) {
         super(props)
@@ -90,7 +100,8 @@ export default connect(state => ({
             historySkip,
             selectedCategories,
             selectedUsers,
-            selectedDates
+            selectedDates,
+            historyUpdate
         } = this.props
         if (prevProps.historySkip !== historySkip
             || prevProps.selectedCategories !== selectedCategories
@@ -100,8 +111,8 @@ export default connect(state => ({
                 && !prevProps.selectedDates.dateEnd
                 && selectedDates.dateStart
                 && selectedDates.dateEnd)
+            || (!prevProps.historyUpdate && historyUpdate)
         ) {
-            // l.d('and passed if')
             fetchHistory(this.props)
         }
     }
@@ -111,69 +122,80 @@ export default connect(state => ({
             historyData,
             users,
             dispatch,
-            historySkip,
             categories,
             selectedCategories,
             selectedUsers,
-            selectedDates
+            selectedDates,
+            historyEditId,
+            historyId
         } = this.props
+        const { data, meta } = historyData
+
         const itemsWithTitles = []
-        for (let i = 0; i < historyData.data.length; i += 1) {
+        const sorted = data.sort((a, b) =>
+            new Date(b.date_create).getTime() - new Date(a.date_create).getTime())
+        for (let i = 0; i < sorted.length; i += 1) {
             if (i === 0
-                || timeLib.getStartDate(new Date(historyData.data[i - 1].date_create)).getTime() !==
-                timeLib.getStartDate(new Date(historyData.data[i].date_create)).getTime())
+                || timeLib.getStartDate(new Date(sorted[i - 1].date_create)).getTime() !==
+                timeLib.getStartDate(new Date(sorted[i].date_create)).getTime())
                 itemsWithTitles.push({
                     id: i + 100500, // TODO: normal keys for titles
-                    date_create: historyData.data[i].date_create
+                    date_create: sorted[i].date_create
                 })
-            itemsWithTitles.push(historyData.data[i])
+            itemsWithTitles.push(sorted[i])
         }
 
         const historyRows = itemsWithTitles.map(item => {
             let user = ''
             if (users && users[item.user_id])
                 user = `${users[item.user_id].firstName} ${users[item.user_id].lastName}`
-            return (<Row key={item.id} item={item} user={user} />)
+            return <Row key={item.id}
+                chatId={historyId}
+                item={item}
+                user={user}
+                categories={categories}
+                editId={historyEditId}
+                dispatch={dispatch} />
         })
-        return (
-            <div className="table-history">
-                <div className="table-header">
-                    <div className="table-header-row">
-                        <Navigation props={this.props} />
-                    </div>
-                    <div className="table-header-row table-header-filters">
-                        <Categories
-                            categories={categories}
+        return <div className="table-history">
+            <div className="table-header">
+                <div className="table-header-row">
+                    <Navigation props={this.props} />
+                </div>
+                <div className="table-header-row table-header-filters">
+                    <Categories
+                        categories={categories}
+                        activeCategories={meta.activeCategories}
+                        dispatch={dispatch}
+                        selected={selectedCategories} />
+                    <TableHeaderCell classes={['padding-right-7']}>
+                        <Users
+                            users={users}
                             dispatch={dispatch}
-                            selected={selectedCategories} />
-                        <TableHeaderCell classes={['padding-right-7']}>
-                            <Users
-                                users={users}
-                                dispatch={dispatch}
-                                selected={selectedUsers} />
-                            <Dates
-                                selected={selectedDates}
-                                dispatch={dispatch}
-                                doUpdate={() => fetchHistory(this.props)} />
-                        </TableHeaderCell>
-                    </div>
+                            selected={selectedUsers}
+                            activeUsersIds={meta.activeUsersIds} />
+                        <Dates
+                            selected={selectedDates}
+                            dispatch={dispatch} />
+                    </TableHeaderCell>
                 </div>
-                <div className="table-content">
-                    {historyRows}
-                </div>
-            </div >
-        )
+            </div>
+            <div className="table-content">
+                {historyRows}
+            </div>
+        </div >
     }
 })
 
 const Navigation = ({ props }) => {
     // l.d('History.Navigation()')
     const { selectedDates,
+        historyEditUndo,
+        historyEditId,
         dispatch = () => { },
         historySkip = 0,
         historyData = { meta: { lengh: 0 }, data: [] }
     } = props
-    // const propsFixed = { ...props }
     const { meta, data } = historyData
     const historyLength = meta.length
 
@@ -195,11 +217,7 @@ const Navigation = ({ props }) => {
                         timeLib.dateString(timeLib.getMonthEndDate(start))
                     dispatch(historyDateSet(dateEnd, false))
                     dispatch(historySkipAction(0))
-                    fetchHistory({
-                        ...props,
-                        historySkip: 0,
-                        selectedDates: { dateStart, dateEnd }
-                    })
+                    dispatch(historyShouldUpdate())
                 }
             }} />
         <input disabled={historySkip === -1 || (historyLength <= data.length + historySkip)}
@@ -241,11 +259,7 @@ const Navigation = ({ props }) => {
                     const dateEnd = timeLib.dateString(end)
                     dispatch(historyDateSet(dateEnd, false))
                     dispatch(historySkipAction(0))
-                    fetchHistory({
-                        ...props,
-                        historySkip: 0,
-                        selectedDates: { dateStart, dateEnd }
-                    })
+                    dispatch(historyShouldUpdate())
                 }
             }} />
         <input
@@ -260,21 +274,27 @@ const Navigation = ({ props }) => {
                 dispatch(historyDateSet(dateStart))
                 dispatch(historyDateSet(dateEnd, false))
                 dispatch(historySkipAction(0))
-                fetchHistory({
-                    ...props,
-                    historySkip: 0,
-                    selectedDates: { dateStart, dateEnd }
-                })
+                dispatch(historyShouldUpdate())
+            }} />
+        <input
+            type="button" value="undo"
+            disabled={!historyEditUndo || !historyEditUndo.chatId || historyEditId}
+            onClick={() => {
+                dispatch(historySave(historyEditUndo.chatId,
+                    historyEditUndo.id,
+                    historyEditUndo.changes))
             }} />
     </TableHeaderCell>
 }
 
-const Categories = ({ categories, dispatch, selected, }) => {
+const Categories = ({ categories, dispatch, selected, activeCategories }) => {
     let cell0 = [],
         cell1,
         cell2
+    const actCategories = activeCategories || []
     const categoryMapper = category => <div key={category.id}>
         <CheckBoxStateless key={category.id} title={category.title}
+            classes={[actCategories.indexOf(category.title) === -1 ? 'color-grey-light' : '']}
             checked={selected.indexOf(category.title) > -1}
             onClick={() => {
                 dispatch(historySkipAction(0))
@@ -291,7 +311,7 @@ const Categories = ({ categories, dispatch, selected, }) => {
                     dispatch(historyCategoriesSelected())
                 }
             }} />
-            <CheckBoxStatefull classes={['margin-left-8']} title='uncat' stateUpdate={() => {
+            <CheckBoxStatefull classes={['margin-left-8', actCategories.indexOf('uncat') === -1 ? 'color-grey-light' : '']} title='uncat' stateUpdate={() => {
                 dispatch(historySkipAction(0))
                 dispatch(historyCategoryToggle('uncat'))
             }} />
@@ -310,21 +330,24 @@ const Categories = ({ categories, dispatch, selected, }) => {
         {cell2 && <TableHeaderCell classes={['padding-right-7']} key={2}>{cell2}</TableHeaderCell>}</span>
 }
 
-const Users = ({ users, dispatch, selected }) => {
+const Users = ({ users, dispatch, selected, activeUsersIds }) => {
     if (!users) return null
     const selUsers = Array.isArray(selected) ? selected : []
     const usersIds = Object.keys(users)
+    const actUsersIds = activeUsersIds || []
     return <div>
         {usersIds.map(id => <div key={id}><CheckBoxStateless key={id} title={`${users[id].firstName} ${users[id].lastName}`}
+            classes={[actUsersIds.indexOf(+id) === -1 ? 'color-grey-light' : '']}
             checked={selUsers.indexOf(id) > -1}
             onClick={() => {
                 dispatch(historySkipAction(0))
                 dispatch(historyUserToggle(id))
-            }} /></div>)}
+            }} /></div>
+        )}
     </div>
 }
 
-const Dates = ({ dispatch, selected, doUpdate }) => {
+const Dates = ({ dispatch, selected }) => {
     if (!selected) return null
     const { dateStart, dateEnd } = selected
     const start = checkDateInput(dateStart)
@@ -336,7 +359,13 @@ const Dates = ({ dispatch, selected, doUpdate }) => {
             onChange={value => dispatch(historyDateSet(String.trim(value)))}
             onBlur={() => {
                 dispatch(historySkipAction(0))
-                doUpdate()
+                dispatch(historyShouldUpdate())
+            }}
+            onKeyPress={char => {
+                if (char === 13) {
+                    dispatch(historySkipAction(0))
+                    dispatch(historyShouldUpdate())
+                }
             }}
         />{start ? `  ${start.toDateString()}` : null}</div>
         <div><Input value={timeLib.dateString(dateEnd)} placeholder={end ? timeLib.dateString(timeLib.getStartDate(end)) : ''}
@@ -344,39 +373,109 @@ const Dates = ({ dispatch, selected, doUpdate }) => {
             onChange={value => dispatch(historyDateSet(String.trim(value), false))}
             onBlur={() => {
                 dispatch(historySkipAction(0))
-                doUpdate()
+                dispatch(historyShouldUpdate())
+            }}
+            onKeyPress={char => {
+                if (char === 13) {
+                    dispatch(historySkipAction(0))
+                    dispatch(historyShouldUpdate())
+                }
             }} />{end ? `  ${end.toDateString()}` : null}</div>
     </div>
 }
 
-const Row = ({ item, user }) => {
+const Row = ({ chatId, item, user, categories, editId, dispatch }) => {
     if (!user) {
         return (<div className="table-row-title">
             {timeLib.dateString(new Date(item.date_create))}</div>)
     }
-    return (
-        <div className={classNames('table-row', {
-            'table-row-deleted': !!item.date_delete
-        })}>
-            <div className="table-cell" style={{ color: 'grey' }}>
-                {item.id}
-            </div>
-            <div className="table-cell" style={{ paddingLeft: '7px' }}>
-                {timeLib.dateTimeString(new Date(item.date_create))}
-            </div>
-            <div className={classNames('table-cell', {
-                warning: item.category === 'uncat' && !item.date_delete
-            })} style={{ paddingLeft: '7px', width: '130px' }}>
-                {item.category}
-            </div>
-            <div className="table-cell" style={{ width: '65px' }}>
-                {item.value}
-            </div>
-            <div className="table-cell" style={{ width: '150px' }}>
-                {user}
-            </div>
-            <div className="table-cell">
-                {item.comment}
-            </div>
-        </div>)
+    const isEdit = editId === item.id
+    return <div className={classNames('table-row', {
+        'table-row-deleted': !!item.date_delete
+    })} onClick={() => { dispatch(historyEditSwitch(item.id)) }}>
+        <div className="table-cell" style={{ color: 'grey' }}>
+            {item.id}
+        </div>
+        <div className="table-cell" style={{ paddingLeft: isEdit ? '' : '7px' }}>
+            {!isEdit && <span>{timeLib.dateTimeString(new Date(item.date_create))} </span>}
+            {isEdit && <InputStatefull
+                defaultValue={timeLib.dateTimeString(new Date(item.date_create))}
+                classes={['width-125']}
+                onBlur={dateString => {
+                    const dt = timeLib.getDateTime(dateString)
+                    if (dt) {
+                        dispatch(historySaveUndo(chatId, item.id,
+                            { date_create: item.date_create }))
+                        dispatch(historySave(chatId, item.id, { date_create: dt }))
+                    }
+                }}
+                onKeyPress={(char, dateString) => {
+                    if (char === 13) {
+                        const dt = timeLib.getDateTime(dateString)
+                        dispatch(historySaveUndo(chatId, item.id,
+                            { date_create: item.date_create }))
+                        dispatch(historySave(chatId, item.id, { date_create: dt }))
+                        dispatch(historyEditSwitch())
+                    }
+                }} />}
+        </div>
+        <div className={classNames('table-cell', {
+            warning: item.category === 'uncat' && !item.date_delete
+        })} style={{ paddingLeft: isEdit ? '' : '7px', width: '140px' }}>
+            {!isEdit && <span>{item.category}</span>}
+            {isEdit && <Select items={categories.map(cat => cat.title)}
+                selected={item.category}
+                onChange={value => {
+                    dispatch(historySaveUndo(chatId, item.id, { category: item.category }))
+                    dispatch(historySave(chatId, item.id, { category: value }))
+                }} />}
+        </div>
+        <div className="table-cell" style={{ width: '65px' }}>
+            {!isEdit && <span>{item.value ? item.value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ') : ''}</span>}
+            {isEdit && <InputStatefull defaultValue={item.value}
+                classes={['width-55']}
+                onBlur={value => {
+                    const parser = new Parser()
+                    try {
+                        const text = parser.parse(value).evaluate()
+                        dispatch(historySaveUndo(chatId, item.id, { value: item.value }))
+                        dispatch(historySave(chatId, item.id, { value: text }))
+                    } catch (ex) {
+                        // no-op
+                    }
+                }}
+                onKeyPress={(char, value) => {
+                    if (char === 13) {
+                        const parser = new Parser()
+                        try {
+                            const text = parser.parse(value).evaluate()
+                            dispatch(historySaveUndo(chatId, item.id, { value: item.value }))
+                            dispatch(historySave(chatId, item.id, { value: text }))
+                        } catch (ex) {
+                            // no-op
+                        }
+                        dispatch(historyEditSwitch())
+                    }
+                }} />}
+        </div>
+        <div className="table-cell" style={{ width: '150px' }}>
+            <span>{user}</span>
+        </div>
+        <div className="table-cell">
+            {!isEdit && <span>{item.comment} </span>}
+            {isEdit && <InputStatefull defaultValue={item.comment}
+                classes={['width-320']}
+                onBlur={comment => {
+                    dispatch(historySaveUndo(chatId, item.id, { comment: item.comment }))
+                    dispatch(historySave(chatId, item.id, { comment }))
+                }}
+                onKeyPress={(char, comment) => {
+                    if (char === 13) {
+                        dispatch(historySaveUndo(chatId, item.id, { comment: item.comment }))
+                        dispatch(historySave(chatId, item.id, { comment }))
+                        dispatch(historyEditSwitch())
+                    }
+                }} />}
+        </div>
+    </div>
 }
