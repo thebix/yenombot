d8611329f12bd44ce32591b4d2a4be426a02ce9b
Pavel Biriukov <syntheticforms@gmail.com>
2018-01-20 17:39:13 +0300

    added storage save to filesystem

--- src/storage.js
+++ src/storage.js
@@ -3,20 +3,57 @@
 import { Observable, BehaviorSubject } from 'rxjs'
 import lib from './lib/root'
 import config from './config';
+import { log, logLevel } from './logger';
 
 class Storage {
     constructor() {
         this.storage = {}
         this.isFsLoadedBehaviorSubject = new BehaviorSubject(false)
         // load saved storage from fs
-        // TODO: check if directory and file exists, if not - create
-        // TODO: add subscription to composite subscription and unsubscribe
-        lib.fs.readJson(config.fileState)
-            .do(fileStorage => {
-                this.storage = fileStorage
+        // check if directory and file exists, if not - create
+        lib.fs.isExists(config.dirStorage)
+            .switchMap(isStorageDirExists => {
+                if (isStorageDirExists !== true) {
+                    log(`Storage:constructor: storage directory doesn't exists, creating. path: <${config.dirStorage}>`, logLevel.INFO)
+                    return lib.fs.mkDir(config.dirStorage)
+                        .switchMap(() => lib.fs.isExists(config.fileState))
+                        .catch(error => {
+                            throw new Error(`Storage:constructor: can't create storage directory. path: <${config.dirStorage}>. error: <${error}>`)
+                        })
+                }
+                return lib.fs.isExists(config.fileState)
             })
-            .map(() => true)
-            .subscribe(this.isFsLoadedBehaviorSubject)
+            .switchMap(isStateFileExists => {
+                if (isStateFileExists !== true) {
+                    log(`Storage:constructor: state file doesn't exists, creating. path: <${config.fileState}>`, logLevel.INFO)
+                    return lib.fs.saveJson(config.fileState, {})
+                        .map(() => true)
+                        .catch(error => {
+                            throw new Error(`Storage:constructor: can't create state file. path: <${config.fileState}>. error: <${error}>`)
+                        })
+                }
+                return Observable.of(true)
+            })
+            .switchMap(isStorageDirAndStateFileExists => {
+                if (isStorageDirAndStateFileExists)
+                    return lib.fs.readJson(config.fileState)
+                        .do(fileStorage => {
+                            this.storage = fileStorage
+                        })
+                        .map(() => true)
+                        .catch(error => {
+                            throw new Error(`Storage:constructor: can't read state file. path: <${config.fileState}>. error: <${error}>`)
+                        })
+                return Observable.of(false)
+            })
+            // TODO: add subscription to composite subscription and unsubscribe
+            .subscribe(
+            initResult => {
+                this.isFsLoadedBehaviorSubject.next(initResult)
+            },
+            initError => {
+                log(initError, logLevel.ERROR)
+            })
     }
     isInitialized() {
         return this.isFsLoadedBehaviorSubject.asObservable()
@@ -26,38 +63,61 @@ class Storage {
     }
     getItem(id, field) {
         if (!this.storage[id] || !this.storage[id][field])
-            return null
-        // TODO: ?make observable?
-        return this.storage[id]
+            return Observable.of(null)
+        return Observable.of(this.storage[id])
     }
     updateItem(id, fieldName, item) {
-        // TODO: Should be extended with save/load from filesystem to save state on restart
-        // TODO: rollback changes to fs storage to previous values on error
         const field = fieldName || '0'
         if (!this.storage[id])
             this.storage[id] = {}
         this.storage[id][field] = item
-        return Observable.of(true)
+        const oldValue = Object.assign({}, this.storage[id][field])
+        return lib.fs.saveJson(config.fileState, this.storage)
+            .map(() => true)
+            .catch(error => {
+                log(`Storage:updateItem: can't save to state file. path: <${config.fileState}>, error:<${error}>`, logLevel.ERROR)
+                // rollback changes to fs storage to previous values on error
+                this.storage[id][field] = oldValue
+                return Observable.of(false)
+            })
     }
     // itemsArray = [{fieldName, item}]
     updateItems(id, itemsArray = []) {
         if (!this.storage[id])
             this.storage[id] = {}
-        // TODO: Should be extended with save/load from filesystem to save state on restart
-        // TODO: rollback changes to fs storage to previous values on error
+        const oldValues = {}
         itemsArray.forEach(itemToSave => {
             const { fieldName, item } = itemToSave
             const field = fieldName || '0'
+            oldValues[field] = Object.assign({}, this.storage[id][field])
             this.storage[id][field] = item
         })
-
-        return Observable.of(true)
+        return lib.fs.saveJson(config.fileState, this.storage)
+            .map(() => true)
+            .catch(error => {
+                log(`Storage:updateItems: can't save to state file. path: <${config.fileState}>, error:<${error}>`, logLevel.ERROR)
+                // rollback changes to fs storage to previous values on error
+                itemsArray.forEach(itemToSave => {
+                    const { fieldName } = itemToSave
+                    const field = fieldName || '0'
+                    this.storage[id][field] = oldValues[field]
+                })
+                return Observable.of(false)
+            })
     }
     removeItem(id, fieldName) {
         const field = fieldName || '0'
         if (this.storage[id]) {
-            // TODO: Should be extended with save/load from filesystem to save state on restart
+            const oldValue = Object.assign({}, this.storage[id][field])
             delete this.storage[id][field]
+            return lib.fs.saveJson(config.fileState, this.storage)
+                .map(() => true)
+                .catch(error => {
+                    log(`Storage:removeItem: can't save to state file. path: <${config.fileState}>, error:<${error}>`, logLevel.ERROR)
+                    // rollback changes to fs storage to previous values on error
+                    this.storage[id][field] = oldValue
+                    return Observable.of(false)
+                })
         }
         return Observable.of(true)
     }
