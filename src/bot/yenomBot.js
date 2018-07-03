a70485c227ed6bc1c67012764fe12b88d8c6b115
Pavel Biriukov <syntheticforms@gmail.com>
2018-07-03 20:13:45 +0300

    commented out monthly stat timer. timer bug should be fixed

--- src/bot/yenomBot.js
+++ src/bot/yenomBot.js
@@ -64,6 +64,7 @@ export default () => {
         .mergeMap(mapUserActionToBotMessages)
         .mergeMap(mapBotMessageToSendResult)
     weeklyIntervalTimer.start()
-    monthlyIntervalTimer.start()
+    // TODO: removed monthly timer in order to avoid timer bug. Timer class should be refactored. error: (node:28334) TimeoutOverflowWarning: 2433606194 does not fit into a 32-bit signed integer. Timeout duration was set to 1  // eslint-disable-line max-len
+    // monthlyIntervalTimer.start()
     return Observable.merge(userTextObservalbe, userActionsObservable)
 }
